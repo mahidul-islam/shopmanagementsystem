ce81c2c26b4087adee613405a0e2c8433983c548
Zihan <mizihan84@gmail.com>
2018-09-02 12:50:11 +0600

    first commit

---
+++ src/items/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+import uuid
+from django.db import models
+from django.conf import settings
+
+class BaseItem(models.Model):
+    name = models.CharField("Item name", max_length=100)
+    slug = models.UUIDField(default=uuid.uuid4, blank=True, editable=False)
+    # Add more user profile fields here. Make sure they are nullable
+    # or with default values
+    picture = models.ImageField('Item picture',
+                                upload_to='profile_pics/%Y-%m-%d/',
+                                null=True,
+                                blank=True)
+    description = models.CharField("Short description", max_length=200, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Medicine(BaseItem):
+    def __str__(self):
+        return "{} item's information". format(self.name)
